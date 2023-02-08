32da1128b5967129d57a801f66682a02aa34e548
DooConCoder <doocon1111@gmail.com>
2023-02-08 12:51:44 +0900

    Add Readme.txt

--- ImagePrint.js
+++ ImagePrint.js
@@ -2,7 +2,16 @@ var imageRequest = new XMLHttpRequest();
 imageRequest.open("GET", "./imageList.json");
 imageRequest.onreadystatechange = function() {
     if (this.readyState == 4) {
-        console.log(this.response);
+        //console.log(this.response);
+        var data = JSON.parse(this.response);
+        for (var i = 0; i < data.length; i++) {
+            var div = document.createElement("div");
+            div.setAttribute("class", "image");
+            var img = document.createElement("img");
+            img.src = data[i];
+            div.appendChild(img);
+            document.body.appendChild(div);
+        }
     }
 }
 imageRequest.send();
