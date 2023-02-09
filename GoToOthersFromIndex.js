0d8ca28ef4940c761682fd9055c4099878fc8d83
DooConCoder <doocon1111@gmail.com>
2023-02-09 17:06:17 +0900

    Add Readme.txt

---
+++ GoToOthersFromIndex.js
@@ -0,0 +1,43 @@
+function imageButtonAverageClick(){
+    window.location.href = './Average.html'
+}
+
+function imageButtonBugsReportClick(){
+    window.location.href = './BugsReport.html'
+}
+
+function imageButtonRightTriangleLengthClick(){
+    window.location.href = './RightTriangleLength.html'
+}
+
+function imageButtonQuadraticEquationClick(){
+    window.location.href = './QuadraticEquation.html'
+}
+
+function imageButtonPrimeNumberDistinctionClick(){
+    window.location.href = './PrimeNumberDistinction.html'
+}
+
+function imageButtonLinearEquationClick(){
+    window.location.href = './LinearEquation.html'
+}
+
+function imageButtonStandardDeviationClick(){
+    window.location.href = './StandardDeviation.html'
+}
+
+function imageButtonFourFundamentalArithmeticOperationsCalculationClick(){
+    window.location.href = './FourFundamentalArithmeticOperationsCalculation.html'
+}
+
+function imageButtonCubicEquationClick(){
+    window.location.href = './CubicEquation.html'
+}
+
+function imageButtonAreaOfTriangle1Click(){
+    window.location.href = './AreaOfTriangle1.html'
+}
+
+function imageButtonAreaOfTriangle2Click(){
+    window.location.href = './AreaOfTriangle2.html'
+}
--- ImagePrint.js
+++ ImagePrint.js
@@ -1,22 +1,3 @@
-var imageRequest = new XMLHttpRequest();
-imageRequest.open("GET", "./imageList.json");
-imageRequest.onreadystatechange = function() {
-    if (this.readyState == 4) {
-        //console.log(this.response);
-        var data = JSON.parse(this.response);
-        for (var i = 0; i < data.length; i++) {
-            var div = document.createElement("div");
-            div.setAttribute("class", "image");
-
-            div.onclick = function() {
-                this.classList.toggle("image-selected")
-            };
-
-            var img = document.createElement("img");
-            img.src = data[i];
-            div.appendChild(img);
-            document.body.appendChild(div);
-        }
-    }
-}
-imageRequest.send();
+div.onclick = function() {
+    this.classList.toggle("image-selected")
+};
